c51cf6ad9e789a5d4927da6b58784c2a75984f27
Saulius Mockus <sm.euro@gmail.com>
2019-03-07 00:07:40 +0200

    pataisyta snieguoles salyga siek tiek.

---
+++ CodeJam/Problems/DigitsPaintingProblem.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeJam.Problems
+{
+    public class DigitsPaintingProblem : IProblem
+    {
+        public string TaskId => "dazymas";
+
+        public string Input => @"5
+5
+101
+102
+5003
+261060
+25665";
+
+        public bool CheckOutput(string answer)
+        {
+            if (string.IsNullOrWhiteSpace(answer))
+            {
+                return false;
+            }
+
+            answer = answer.Trim(new char[] { '\r', '\n', ' ' });
+            if (Input == answer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- CodeJam/Service/CheckAnswers.cs
+++ CodeJam/Service/CheckAnswers.cs
@@ -8,38 +8,12 @@ namespace CodeJam.Service
     {
         private readonly IProblem[] _problems =
         {
-            new SnowWhiteProblem()
+            new SnowWhiteProblem(),
+            new DigitsPaintingProblem()
         };
 
         public bool IsAnswerCorrect(string taskId, string answer)
         {
-            answer = answer.Trim(new char[] { '\r', '\n', ' '  });
-            switch (taskId)
-            {
-                case "snieguole":
-                    break;
-                case "dazymas":
-                    string correct = 
-@"5
-5
-101
-102
-5003
-261060
-25665";
-                    if (correct == answer)
-                    {
-                        return true;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                default:
-                    return false;
-            }
-
-            return false;
             var problem = _problems.FirstOrDefault(p => p.TaskId.Equals(taskId, StringComparison.OrdinalIgnoreCase));
             if (problem == null) throw new ArgumentException(nameof(taskId));
 
