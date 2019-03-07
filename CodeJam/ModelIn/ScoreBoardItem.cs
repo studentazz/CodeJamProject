bfbde431017dcc30d0567116eebecd7cb9ca245e
jonas <jonas@trafi.com>
2019-03-07 12:46:36 +0200

    add ScoreBoardItem

---
+++ CodeJam/ModelIn/ScoreBoardItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CodeJam.ModelIn
+{
+    public class ScoreBoardItem
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+        public TimeSpan Penalty { get; set; }
+
+        public bool SnowwhiteSolved { get; set; }
+        public int SnowwhiteIncorrectAttempts { get; set; }
+        public TimeSpan SnowwhiteSolvingTime { get; set; }
+
+        public bool ElectionSolved { get; set; }
+        public int ElectionIncorrectAttempts { get; set; }
+        public TimeSpan ElectionSolvingTime { get; set; }
+    }
+}
